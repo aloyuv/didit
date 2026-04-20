0a7d826f0a98cf3fa3016a8e1ad751b7424a18bb
Yuval Greenfield <ubershmekel@gmail.com>
2026-04-20 12:28:05 -0700

    Settings screen links to github

--- app/lib/features/settings/settings_screen.dart
+++ app/lib/features/settings/settings_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
@@ -22,6 +23,16 @@ class SettingsScreen extends StatelessWidget {
             leading: const Icon(Icons.download),
             title: const Text('Import data'),
             onTap: () {},
+          ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.code),
+            title: const Text('See source on GitHub'),
+            subtitle: const Text('github.com/aloyuv/didit'),
+            onTap: () => launchUrl(
+              Uri.parse('https://github.com/aloyuv/didit/'),
+              mode: LaunchMode.externalApplication,
+            ),
           ),
           const Divider(),
           const ListTile(
